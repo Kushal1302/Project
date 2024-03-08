1cfe9a799a2c868c03e4ff0ffa24932dff328147
Kushal Kumar <kushalmalviya1302@gmail.com>
2024-03-08 23:01:08 +0530

    Some changes

--- server/app.js
+++ server/app.js
@@ -5,22 +5,9 @@ import userRouter from './Controllers/user.js'
 
 const app = express()
 app.get('/' , async (req , res) => {
-    try {
-        const users = await prisma.user.create({
-            data:{
-                email:"Kushalmalviya1302@gmail.com",
-                name:"Kushal Kumar"
-            }
-        })
-        return res.json({
-            message:"done",
-            data:users
-        })
-    } catch (error) {
-        return res.json({
-            message:error.message
-        })
-    }
+    res.json({
+        message:"Hello And Welcome"
+    })
 })
 app.use('/find' , userRouter)
 const port = process.env.PORT || 8000
